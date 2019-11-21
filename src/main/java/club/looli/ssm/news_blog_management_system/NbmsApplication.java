3ea09e1dd86a6a9946f6bdb78cd42e0f8ce82075
looljs <2891934338@qq.com>
2019-11-21 21:35:49 +0800

    测试提交

---
+++ src/main/java/club/looli/ssm/news_blog_management_system/NbmsApplication.java
@@ -0,0 +1,16 @@
+package club.looli.ssm.news_blog_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 新闻博客管理系统
+ */
+@SpringBootApplication
+public class NbmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbmsApplication.class, args);
+    }
+
+}
